860c74d5450e0cd0d5f054d616fdedfe925c3148
bennycive <bennycive@gmail.com>
2024-08-22 01:28:51 +0300

    javascript , responsive sidebar and card image transform on hover

---
+++ public/main.js
@@ -0,0 +1,11 @@
+// JAVASCRIPT CODE HERE
+const menuIcon = document.querySelector("#menuIcon");
+const menu = document.querySelector("#menu");
+
+menuIcon.addEventListener("click", () => {
+  if (menu.classList.contains("hidden")) {
+    menu.classList.remove("hidden");
+  } else {
+    menu.classList.add("hidden");
+  }
+});
